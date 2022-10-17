003260ce61abf33daf09ae882f309de75c44e0c6
HHK <hhk.contact@protonmail.com>
2022-10-17 15:15:52 +0200

    feat: add gelato ops mock

--- src/mock/GelatoMock.sol
+++ src/mock/GelatoMock.sol
@@ -1,18 +1,35 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity ^0.8.16;
 
-contract GelatoMock {
-    uint256 public fee;
-    address public feeToken;
+import {IOps} from "./../interfaces/IOps.sol";
 
-    function getFeeDetails() public view returns (uint256, address) {
-        return (fee, feeToken);
+contract GelatoMock is IOps {
+    address payable internal _gelato;
+    address internal _feeToken;
+    uint256 internal _fee;
+
+    constructor(
+        address payable _newGelato,
+        address _newFeeToken,
+        uint256 _newFee
+    ) {
+        _gelato = _newGelato;
+        _feeToken = _newFeeToken;
+        _fee = _newFee;
+    }
+
+    function gelato() external view returns (address payable) {
+        return _gelato;
+    }
+
+    function getFeeDetails() external view returns (uint256, address) {
+        return (_fee, _feeToken);
     }
 
     function createTask(
-        address target,
-        bytes calldata targetSelector,
-        address checker,
-        bytes calldata checkerSelector
-    ) public {}
+        address _execAddress,
+        bytes4 _execSelector,
+        address _resolverAddress,
+        bytes calldata _resolverData
+    ) external returns (bytes32 task) {}
 }
